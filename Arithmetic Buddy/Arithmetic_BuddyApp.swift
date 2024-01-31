9192383c2c8ae30b9d12e3bee47aff6fda3b024b
Eason Ong <eong@lcs.on.ca>
2024-01-31 11:42:36 -0500

    Initial Commit

---
+++ Arithmetic Buddy/Arithmetic_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_BuddyApp.swift
+//  Arithmetic Buddy
+//
+//  Created by Ong Eason on 31/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
