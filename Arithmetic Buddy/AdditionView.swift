ac40bef19c545b65852dc86862aa944948fbdca6
Eason Ong <eong@lcs.on.ca>
2024-01-31 11:44:22 -0500

    Created the project and finished initial project organization.

---
+++ Arithmetic Buddy/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  Arithmetic Buddy
+//
+//  Created by Ong Eason on 31/1/2024.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
